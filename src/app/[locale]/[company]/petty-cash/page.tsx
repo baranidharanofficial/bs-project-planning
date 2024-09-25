890610d16561964d2246a70209fc31d69d3c35c5
Baranidharan <baranidharanofficial@gmail.com>
2024-09-25 13:41:13 +0530

    m

--- src/app/[locale]/[company]/petty-cash/page.tsx
+++ src/app/[locale]/[company]/petty-cash/page.tsx
@@ -35,6 +35,7 @@ export default function IndexPage({ params: { locale } }: Props) {
           { user_id },
           {
             headers: {
+              "Content-Type": "application/json",
               Cookie: `sid=${sid}`,
             },
           }
